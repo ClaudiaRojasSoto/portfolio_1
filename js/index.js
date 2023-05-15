3fa12e593a320738f84701fa21801ca22bd614b1
iturres <emanueliturres.co@gmail.com>
2023-05-15 18:29:26 -0300

    Add 'mobile-menu's 'index.js' -script-tag- on 'index.html' -file-

--- js/index.js
+++ js/index.js
@@ -1,39 +1,71 @@
-function toggleMenu() {
-  const nav = document.querySelector('.my-nav');
-  const navMobile = document.querySelector('.nav-mobile');
+const mobileMenuModulesCreator = (() => {
+  const nav = document.querySelector(".my-nav");
 
-  if (!nav.classList.contains('menu-open')) {
-    nav.classList.add('menu-open');
+  let closeIcon;
+  let menuMobile;
+  let menuItems;
 
-    const closeIcon = document.createElement('div');
-    closeIcon.classList.add('close-icon');
-    closeIcon.textContent = 'x';
+  const menuItemsHTML = `
+    <li><a href="#portfolio">Portfolio</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#contact">Contact</a></li>
+  `;
 
-    closeIcon.addEventListener('click', () => {
-      nav.classList.remove('menu-open');
-      if (nav.contains(navMobile)) {
-        nav.removeChild(navMobile);
-      }
-    });
-
-    const menuMobile = document.createElement('div');
-    menuMobile.classList.add('nav-mobile');
-
-    const menuItems = document.createElement('ul');
-    menuItems.classList.add('menu-list');
-    menuItems.innerHTML = `
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
-      `;
-  } else {
-    nav.classList.remove('menu-open');
-    const menuMobile = document.querySelector('.nav-mobile');
-    if (nav.contains(menuMobile)) {
-      nav.removeChild(menuMobile);
+  function removeNavMobile() {
+    const navMobile = document.querySelector(".nav-mobile");
+    if (nav.contains(navMobile)) {
+      nav.removeChild(navMobile);
     }
   }
-}
 
-const menuToggle = document.querySelector('.menu-toggle');
-menuToggle.addEventListener('click', toggleMenu);
+  function createMenuItems() {
+    menuItems = document.createElement("ul");
+    menuItems.classList.add("menu-list");
+    menuItems.innerHTML = menuItemsHTML;
+    menuItems.querySelectorAll("a").forEach((item) => {
+      item.addEventListener("click", function (event) {
+        event.preventDefault();
+        const targetSection = document.querySelector(item.getAttribute("href"));
+        targetSection.scrollIntoView({
+          behavior: "smooth",
+        });
+        mobileMenuModulesCreator.closeMenu();
+      });
+    });
+  }
+
+  function createCloseIcon() {
+    closeIcon = document.createElement("div");
+    closeIcon.classList.add("close-icon");
+    closeIcon.textContent = "x";
+    closeIcon.addEventListener("click", mobileMenuModulesCreator.closeMenu);
+  }
+
+  function createMenuMobile() {
+    menuMobile = document.createElement("div");
+    menuMobile.classList.add("nav-mobile");
+    menuMobile.appendChild(closeIcon);
+    menuMobile.appendChild(menuItems);
+  }
+
+  function openMenu() {
+    nav.classList.add("menu-open");
+    createCloseIcon();
+    createMenuItems();
+    createMenuMobile();
+    nav.appendChild(menuMobile);
+  }
+
+  function closeMenu() {
+    nav.classList.remove("menu-open");
+    removeNavMobile();
+  }
+  return {
+    openMenu,
+    closeMenu,
+  };
+})();
+const menuToggleModule = (() => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  menuToggle.addEventListener("click", mobileMenuModulesCreator.openMenu);
+})();
