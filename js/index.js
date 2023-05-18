7ba7a0f2ecbc76c1cd4694838c6d40b4b72801d3
iturres <emanueliturres.co@gmail.com>
2023-05-18 12:47:01 -0300

    Add 'formData' obj in 'formValidator' -function-

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ const mobileMenuModulesCreator = (() => {
         event.preventDefault();
         const targetSection = document.querySelector(item.getAttribute('href'));
         targetSection.scrollIntoView({
-          behavior: 'smooth',
+          behavior: 'smooth'
         });
         mobileMenuModulesCreator.closeMenu();
       });
@@ -65,7 +65,7 @@ const mobileMenuModulesCreator = (() => {
   }
   return {
     openMenu,
-    closeMenu,
+    closeMenu
   };
 })();
 
@@ -97,49 +97,134 @@ function getData(id) {
 }
 
 function openPopUp(project) {
-  const modal = createElement('div', { classList: ['modal'], id: 'projectDetailsModal' }, [
-    createElement('div', { classList: ['modal-content'] }, [
-      createElement('span', { classList: ['close-btn'], id: 'close-btn-modal', innerHTML: '<img src="img/IconXpop.svg">' }),
-      createElement('div', { classList: ['project-details'] }, [
-        createElement('div', { classList: ['contenedor-title-modal'] }, [
-          createElement('h2', { classList: ['modal-title'], textContent: project.name }),
-        ]),
-        createElement('p', { classList: ['modal-subtitle'] }, [
-          createElement('span', { classList: ['modal-stack-subtitle'], textContent: project.subtitle }),
-          createElement('span', { classList: ['grey'] }, [
-            createElement('img', { src: './img/period.svg' }),
-            createElement('span', { classList: ['modal-stack'], textContent: project.stack }),
-            createElement('img', { src: './img/period.svg' }),
-            createElement('span', { classList: ['modal-year'], textContent: project.year }),
+  const modal = createElement(
+    'div',
+    { classList: ['modal'], id: 'projectDetailsModal' },
+    [
+      createElement('div', { classList: ['modal-content'] }, [
+        createElement('span', {
+          classList: ['close-btn'],
+          id: 'close-btn-modal',
+          innerHTML: '<img src="img/IconXpop.svg">'
+        }),
+        createElement('div', { classList: ['project-details'] }, [
+          createElement('div', { classList: ['contenedor-title-modal'] }, [
+            createElement('h2', {
+              classList: ['modal-title'],
+              textContent: project.name
+            })
           ]),
-        ]),
-        createElement('div', { classList: ['modal-img-container'] }, [
-          createElement('img', { classList: ['modal-img'], src: project.featuredImage }),
-        ]),
-        createElement('div', { classList: ['modal-container-desktop'] }, [
-          createElement('div', { classList: ['modal-container-description'] }, [
-            createElement('p', { classList: ['modal-description'], textContent: project.description }),
-            createElement('p', { classList: ['modal-description'], textContent: project.description2 }),
+          createElement('p', { classList: ['modal-subtitle'] }, [
+            createElement('span', {
+              classList: ['modal-stack-subtitle'],
+              textContent: project.subtitle
+            }),
+            createElement('span', { classList: ['grey'] }, [
+              createElement('img', { src: './img/period.svg' }),
+              createElement('span', {
+                classList: ['modal-stack'],
+                textContent: project.stack
+              }),
+              createElement('img', { src: './img/period.svg' }),
+              createElement('span', {
+                classList: ['modal-year'],
+                textContent: project.year
+              })
+            ])
           ]),
-          createElement('div', { classList: ['modal-container-right-buttons'] }, [
-            createElement('ul', { classList: ['modal-buttons'] },
-              project.technologies.map((tech, index) => createElement('li', { classList: [`modal-${index < 3 ? ['html', 'css', 'js'][index] : 'js'}-li`] }, [
-                createElement('span', { classList: [`modal-${index < 3 ? ['html', 'css', 'js'][index] : 'js'}-span`], textContent: tech }),
-              ]))),
-            createElement('div', { classList: ['line'] }),
-            createElement('div', { classList: ['project-links'], id: 'project-link-modal' }, [
-              createElement('a', { classList: ['project-link'], textContent: 'Live Link', href: project.liveLink }, [
-                createElement('img', { classList: ['image-button1'], src: project.buttonLive }),
-              ]),
-              createElement('a', { classList: ['project-link'], textContent: 'Source Link', href: project.sourceLink }, [
-                createElement('img', { classList: ['image-button2'], src: project.buttonSource }),
-              ]),
-            ]),
+          createElement('div', { classList: ['modal-img-container'] }, [
+            createElement('img', {
+              classList: ['modal-img'],
+              src: project.featuredImage
+            })
           ]),
-        ]),
-      ]),
-    ]),
-  ]);
+          createElement('div', { classList: ['modal-container-desktop'] }, [
+            createElement(
+              'div',
+              { classList: ['modal-container-description'] },
+              [
+                createElement('p', {
+                  classList: ['modal-description'],
+                  textContent: project.description
+                }),
+                createElement('p', {
+                  classList: ['modal-description'],
+                  textContent: project.description2
+                })
+              ]
+            ),
+            createElement(
+              'div',
+              { classList: ['modal-container-right-buttons'] },
+              [
+                createElement(
+                  'ul',
+                  { classList: ['modal-buttons'] },
+                  project.technologies.map((tech, index) =>
+                    createElement(
+                      'li',
+                      {
+                        classList: [
+                          `modal-${
+                            index < 3 ? ['html', 'css', 'js'][index] : 'js'
+                          }-li`
+                        ]
+                      },
+                      [
+                        createElement('span', {
+                          classList: [
+                            `modal-${
+                              index < 3 ? ['html', 'css', 'js'][index] : 'js'
+                            }-span`
+                          ],
+                          textContent: tech
+                        })
+                      ]
+                    )
+                  )
+                ),
+                createElement('div', { classList: ['line'] }),
+                createElement(
+                  'div',
+                  { classList: ['project-links'], id: 'project-link-modal' },
+                  [
+                    createElement(
+                      'a',
+                      {
+                        classList: ['project-link'],
+                        textContent: 'Live Link',
+                        href: project.liveLink
+                      },
+                      [
+                        createElement('img', {
+                          classList: ['image-button1'],
+                          src: project.buttonLive
+                        })
+                      ]
+                    ),
+                    createElement(
+                      'a',
+                      {
+                        classList: ['project-link'],
+                        textContent: 'Source Link',
+                        href: project.sourceLink
+                      },
+                      [
+                        createElement('img', {
+                          classList: ['image-button2'],
+                          src: project.buttonSource
+                        })
+                      ]
+                    )
+                  ]
+                )
+              ]
+            )
+          ])
+        ])
+      ])
+    ]
+  );
 
   document.body.appendChild(modal);
 
@@ -165,30 +250,31 @@ function createCards(projects) {
   const cardList = document.getElementById('portfolio');
 
   projects.forEach((project, index) => {
-    const projectData = window.innerWidth <= 768 ? project : project.desktopData;
+    const projectData =
+      window.innerWidth <= 768 ? project : project.desktopData;
 
     const card = createElement('div', { classList: ['card'] });
 
     const bgImg = createElement('div', {
       classList: [`bg-img${index + 1}`],
-      id: window.innerWidth > 768 && (index === 1 || index === 3) ? 'img2' : '',
+      id: window.innerWidth > 768 && (index === 1 || index === 3) ? 'img2' : ''
     });
     card.appendChild(bgImg);
 
     const img = createElement('img', {
       classList: ['principal-img'],
       src: projectData.featuredImage,
-      alt: 'Project image',
+      alt: 'Project image'
     });
     bgImg.appendChild(img);
 
     const contentCardDesktop = createElement('div', {
-      classList: ['content-card-desktop'],
+      classList: ['content-card-desktop']
     });
     card.appendChild(contentCardDesktop);
 
     const title = createElement('h2', {}, [
-      document.createTextNode(projectData.name),
+      document.createTextNode(projectData.name)
     ]);
     contentCardDesktop.appendChild(title);
 
@@ -197,32 +283,33 @@ function createCards(projects) {
       createElement('span', { classList: ['grey'] }, [
         createElement('img', {
           src: projectData.point,
-          alt: 'Grey period in text',
+          alt: 'Grey period in text'
         }),
         document.createTextNode(projectData.stack),
         createElement('img', {
           src: projectData.point2,
-          alt: 'Grey period in text',
+          alt: 'Grey period in text'
         }),
-        document.createTextNode(projectData.year),
-      ]),
+        document.createTextNode(projectData.year)
+      ])
     ]);
     contentCardDesktop.appendChild(subtitle);
 
     const description = createElement('p', {}, [
-      document.createTextNode(projectData.description),
+      document.createTextNode(projectData.description)
     ]);
     contentCardDesktop.appendChild(description);
 
     const buttonList = createElement('ul', { classList: ['card-buttons'] });
-    const technologies = window.innerWidth <= 768
-      ? projectData.technologies.slice(0, 3)
-      : project.desktopData.technologies.slice(0, 4);
+    const technologies =
+      window.innerWidth <= 768
+        ? projectData.technologies.slice(0, 3)
+        : project.desktopData.technologies.slice(0, 4);
 
     technologies.forEach((technology) => {
       const buttonItem = createElement('li', {});
       const button = createElement('button', {}, [
-        document.createTextNode(technology),
+        document.createTextNode(technology)
       ]);
       buttonItem.appendChild(button);
       buttonList.appendChild(buttonItem);
@@ -231,7 +318,7 @@ function createCards(projects) {
     contentCardDesktop.appendChild(buttonList);
 
     const projectLinkContainer = createElement('div', {
-      classList: ['project-link-container'],
+      classList: ['project-link-container']
     });
     contentCardDesktop.appendChild(projectLinkContainer);
 
@@ -241,9 +328,9 @@ function createCards(projects) {
         id: `projectButton${index + 1}`,
         href: '#',
         classList: ['project-link'],
-        'data-see-project-button': '',
+        'data-see-project-button': ''
       },
-      [document.createTextNode('See project')],
+      [document.createTextNode('See project')]
     );
     projectLink.addEventListener('click', handleProjectButtonClick);
     projectLinkContainer.appendChild(projectLink);
@@ -254,13 +341,16 @@ function createCards(projects) {
 
 createCards(projects);
 
-const seeProjectsButton = document.querySelectorAll('[data-see-project-button]');
+const seeProjectsButton = document.querySelectorAll(
+  '[data-see-project-button]'
+);
 
 seeProjectsButton.forEach((button) => {
   button.addEventListener('click', () => {
     const projectId = button.id;
     const projectData = getData(projectId);
-    const project = window.innerWidth <= 768 ? projectData : projectData.desktopData;
+    const project =
+      window.innerWidth <= 768 ? projectData : projectData.desktopData;
     openPopUp(project);
   });
 });
@@ -268,11 +358,15 @@ seeProjectsButton.forEach((button) => {
 //Validation form
 const formValidator = document.querySelector('.form-contact-me');
 
-formValidator.addEventListener('submit', function(event) {
+formValidator.addEventListener('submit', function (event) {
   event.preventDefault();
   const emailInput = document.querySelector('input[name="email"]');
   const textArea = document.getElementById('id-textarea');
- 
+
+  const formData = {
+    name: document.querySelector('input[name="name"]').value,
+    email: document.querySelector('input[name="email"]').value,
+    textarea: document.getElementById('id-textarea').value
+  };
   const emailValue = emailInput.value;
-  
 });
