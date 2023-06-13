cc353ad308410df45b986cd7cad28d73760ce23e
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-06-13 16:04:30 -0400

    Add pictures and modify the data of the project in portfolio at id number one

--- js/data.js
+++ js/data.js
@@ -1,39 +1,35 @@
 const projects = [
   {
     id: 'projectButton1',
-    name: 'Tonic',
+    name: 'To Do List',
     subtitle: 'Canopy',
     point: './img/period.svg',
     stack: 'Back End Dev',
     point2: './img/period.svg',
-    year: '2015',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    featuredImage: './img/card1.svg',
+    year: '2023',
+    description: 'This project build a To-Do List that uses Webpack for the development environment configuration.\'s The application allows you to manage and keep track of your daily tasks.',
+    featuredImage: './img/todolist0.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
-    liveLink: 'https://claudiarojassoto.github.io/',
-    sourceLink: 'https://github.com/ClaudiaRojasSoto/portfolio_1',
+    liveLink: 'https://claudiarojassoto.github.io/toDo_list/dist/',
+    sourceLink: 'https://github.com/ClaudiaRojasSoto/toDo_list',
     buttonLive: './img/Iconreloj.svg',
     buttonSource: './img/IconGit.svg',
     desktopData: {
-      name: 'Tonic',
+      name: 'To Do List',
       subtitle: 'Canopy',
       point: './img/period.svg',
       stack: 'Back End Dev',
       point2: './img/period.svg',
-      year: '2015',
-      featuredImage: '../img/img1_desk.svg',
+      year: '2023',
+      featuredImage: '../img/img1_desk.png',
       description:
-                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
-                + 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer '
-                + 'took a galley of type and scrambled it 1960s. Lorem <br> Ipsum is simply dummy text of the printing and typesetting industry. '
-                + 'Lorem Ipsum has been the industry\'s ',
-      description2: 'standard dummy text ever since the 1500s, when an unknown printer took a galley of '
-                + 'type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. '
-                + 'Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type '
-                + 'and scrambled it 1960s.',
-      technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
-      liveLink: 'https://claudiarojassoto.github.io/',
-      sourceLink: 'https://github.com/ClaudiaRojasSoto/portfolio_1',
+                'This project build a To-Do List that uses Webpack for the development environment configuration.'
+                + 'The application allows you  \'s to manage and keep track of your daily tasks..',
+      description2: 'This project build a To-Do List that uses Webpack for the development '
+                + 'environment configuration. The application allows you to manage and keep track of your daily tasks.',
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      liveLink: 'https://claudiarojassoto.github.io/toDo_list/dist/',
+      sourceLink: 'https://github.com/ClaudiaRojasSoto/toDo_list',
       buttonLive: './img/Iconreloj.svg',
       buttonSource: './img/IconGit.svg',
     },
