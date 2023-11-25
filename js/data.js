685fdc959fd43ff3c2bf68a949e829d31f523ef8
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-11-25 18:37:51 -0300

    Change the h2 on the nav and delete the description 2 on the data.js. Add the wellfound link to peace icon

--- js/data.js
+++ js/data.js
@@ -7,7 +7,8 @@ const projects = [
     stack: 'Front End Dev',
     point2: './img/period.svg',
     year: '2023',
-    description: 'This project build a To-Do List that uses Webpack for the development environment configuration.\'s The application allows you to manage and keep track of your daily tasks.',
+    description: 'This project build a To-Do List that uses Webpack for the development '
+    + 'environment configuration. The application allows you to manage and keep track of your daily tasks.',
     featuredImage: './img/todolist0.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
     liveLink: 'https://claudiarojassoto.github.io/toDo_list/dist/',
@@ -25,8 +26,6 @@ const projects = [
       description:
                 'This project build a To-Do List that uses Webpack for the development environment configuration.'
                 + 'The application allows you  \'s to manage and keep track of your daily tasks..',
-      description2: 'This project build a To-Do List that uses Webpack for the development '
-                + 'environment configuration. The application allows you to manage and keep track of your daily tasks.',
       technologies: ['HTML', 'CSS', 'JavaScript'],
       liveLink: 'https://claudiarojassoto.github.io/toDo_list/dist/',
       sourceLink: 'https://github.com/ClaudiaRojasSoto/toDo_list',
@@ -42,7 +41,10 @@ const projects = [
     stack: 'Full stack Dev',
     point2: './img/period.svg',
     year: '2023',
-    description: 'The Budget App is a mobile web application designed for efficient budget management',
+    description: 'The Budget App is a mobile web application designed for efficient budget management'
+    + 'With this app, users can register and log in to maintain their personal financial records'
+    + 'They can add new transactions associated with specific categories and gain insights into their spending'
+    + 'patterns by tracking expenses within each category',
     featuredImage: './img/budgetApp.png',
     technologies: ['HTML', 'CSS', 'RoR'],
     liveLink: 'https://budget-app-047t.onrender.com',
@@ -62,10 +64,6 @@ const projects = [
                 + 'With this app, users can register and log in to maintain their personal financial records'
                 + 'They can add new transactions associated with specific categories and gain insights into their spending'
                 + 'patterns by tracking expenses within each category',
-      description2: 'The Budget App is a mobile web application designed for efficient budget management'
-      + 'With this app, users can register and log in to maintain their personal financial records'
-      + 'They can add new transactions associated with specific categories and gain insights into their spending'
-      + 'patterns by tracking expenses within each category',
       technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
       liveLink: 'https://budget-app-047t.onrender.com',
       sourceLink: 'https://github.com/ClaudiaRojasSoto/BudgetApp',
@@ -81,7 +79,10 @@ const projects = [
     stack: 'Full Stack Dev',
     point2: './img/period.svg',
     year: '2023',
-    description: 'Car Reservations App is a Full Stack Application to reserve cars and test it.',
+    description: 'Car Reservations App is a Full Stack Application to reserve cars and test it. '
+    + 'This project is developed using React.js, designed to provide an intuitive user interface '
+    + 'for managing reservations or appointments for test drives of cars. It communicates with the back-end  '
+    + 'API built with Ruby on Rails. ',
     featuredImage: './img/reserveCar.png',
     technologies: ['HTML', 'CSS', 'JavaScript'],
     liveLink: 'https://claudiarojassoto.github.io/',
@@ -98,10 +99,6 @@ const projects = [
       featuredImage: './img/reserveCar.png',
       description:
                 'Car Reservations App is a Full Stack Application to reserve cars and test it. '
-                + 'This project is developed using React.js, designed to provide an intuitive user interface '
-                + 'for managing reservations or appointments for test drives of cars. It communicates with the back-end  '
-                + 'API built with Ruby on Rails. ',
-      description2: 'Car Reservations App is a Full Stack Application to reserve cars and test it. '
                 + 'This project is developed using React.js, designed to provide an intuitive user interface '
                 + 'for managing reservations or appointments for test drives of cars. It communicates with the back-end  '
                 + 'API built with Ruby on Rails. ',
@@ -140,10 +137,6 @@ const projects = [
                 + 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer '
                 + 'took a galley of type and scrambled it 1960s. Lorem <br> Ipsum is simply dummy text of the printing and typesetting industry. '
                 + 'Lorem Ipsum has been the industry\'s ',
-      description2: 'standard dummy text ever since the 1500s, when an unknown printer took a galley of '
-                + 'type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. '
-                + 'Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type '
-                + 'and scrambled it 1960s.',
       technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
       liveLink: 'https://claudiarojassoto.github.io/',
       sourceLink: 'https://github.com/ClaudiaRojasSoto/portfolio_1',
